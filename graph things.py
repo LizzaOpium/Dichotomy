517181577e827b13c1038b92a817498ca47faee9
LizzaOpium <pleha4862@gmail.com>
2025-12-25 01:30:06 +0300

    Enhance function evaluation and error handling
    Added error handling for undefined functions and improved the evaluation process.

--- graph things.py
+++ graph things.py
@@ -6,20 +6,24 @@ from matplotlib import pyplot as plt
 '''
 
 def f(x):
-    dict = {
-        'x': x,
-        'sin': np.sin,
-        'cos': np.cos,
-        'tan': np.tan,
-        'exp': np.exp,
-        'log': np.log,
-        'sqrt': np.sqrt,
-        'abs': np.abs,
-        'pi': np.pi,
-        'e': np.e,
-        'X': x
-    }
-    return eval(func, dict)
+    try:
+        dict = {
+            'x': x,
+            'sin': np.sin,
+            'cos': np.cos,
+            'tan': np.tan,
+            'exp': np.exp,
+            'log': np.log,
+            'sqrt': np.sqrt,
+            'abs': np.abs,
+            'pi': np.pi,
+            'e': np.e,
+            'X': x
+        }
+        return eval(func, dict)
+    except:
+        return np.nan
+
 
 '''
 Дихтомия, принимает на вход функцию и a, b
@@ -31,6 +35,10 @@ def dichotomy(a, b):
     r = eps/2 # отступы от середины отрезка
     global c # ввел глобальную переменную, чтобы потом ее использовать
 
+    if np.isnan(f(a)) or np.isnan(f(b)):
+        c = None
+        return f'Ошибка: функция не определена на отрезке ({a}; {b})'
+
     while abs(b - a) > eps: # оценка отрезков, пока их длина не станет меньше шага
         c = (a + b) / 2
 
@@ -41,6 +49,10 @@ def dichotomy(a, b):
         else:
             a = c
     # вывод данных
+    if np.isnan(f(c)):
+        c = None
+        return f'Ошибка: функция не определена на отрезке ({a}; {b})'
+
     print('> Найден максимум функции: ')
     # округление сделано до 4 цифр после запятой для большего удобства
     return f'f(x_max) = {round(f(c), 4)}, x_max = {round(c, 4)}'
@@ -66,14 +78,17 @@ print(dichotomy(a, b))
 и пересечением синих пунктиров - есть погрешность
 '''
 
-x = np.linspace(a, b, 1000)
-y = f(x)
+if c == None:
+    print(f'> График не был построен')
+else:
+    x = np.linspace(a, b, 1000)
+    y = f(x)
 
-plt.axhline(y=f(c), color='blue', linestyle='--')
-plt.axvline(x=c, color='blue', linestyle='--')
-plt.plot(x, y, label='f(x)', color = 'red', linestyle = 'solid')
-plt.title('График f(x)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-plt.show()
+    plt.axhline(y=f(c), color='blue', linestyle='--')
+    plt.axvline(x=c, color='blue', linestyle='--')
+    plt.plot(x, y, label='f(x)', color = 'red', linestyle = 'solid')
+    plt.title('График f(x)')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.show()
